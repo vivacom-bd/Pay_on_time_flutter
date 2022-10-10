0e77785d1247bdbadd34be2506459b85abf35253
Mahmud-CSE16 <mahmudul7959@gmail.com>
2022-10-10 17:25:22 +0600

    Update | 10-10-2022

--- lib/Models/pyament_auth_response.dart
+++ lib/Models/pyament_auth_response.dart
@@ -1,6 +1,6 @@
 class PaymentAuthResponse {
   String? jwtToken;
-  int? baseamount;
+  double? baseamount;
   String? transactionNumber;
   String? currencyiso3a;
 
@@ -12,17 +12,17 @@ class PaymentAuthResponse {
 
   PaymentAuthResponse.fromJson(Map<String, dynamic> json) {
     jwtToken = json['jwt_token'];
-    baseamount = json['baseamount'];
+    baseamount = json['baseamount']==null? 0 : json['baseamount']*1.0;
     transactionNumber = json['transaction_number'];
     currencyiso3a = json['currencyiso3a'];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['jwt_token'] = this.jwtToken;
-    data['baseamount'] = this.baseamount;
-    data['transaction_number'] = this.transactionNumber;
-    data['currencyiso3a'] = this.currencyiso3a;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['jwt_token'] = jwtToken;
+    data['baseamount'] = baseamount;
+    data['transaction_number'] = transactionNumber;
+    data['currencyiso3a'] = currencyiso3a;
     return data;
   }
 }
